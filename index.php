28d23912fa5f8b8b702af0e92cbf90b8696d792f
JonathanSC <jonathansolisc_22@hotmail.com>
2019-11-24 23:07:32 -0600

    done initial config for predefined login

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello madafaka";
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Web Datagrip | Jonathan Solis Ceballos</title>
+</head>
+<body>
+  <div>Hello ima div</div>
+  
+</body>
+</html>
